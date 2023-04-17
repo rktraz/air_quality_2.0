04dae300ea84e8ea73b7c20eb1f9d717a7a5ce86
rktraz <roman.kaharlytskyi@gmail.com>
2023-04-17 12:52:42 +0200

    improved parsing algorithm and streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,39 +4,49 @@ from streamlit_folium import st_folium
 from geopy import distance
 import json
 import time
+import pickle
+
 
 def print_fancy_header(text, font_size=22, color="#ff5f27"):
     res = f'<span style="color:{color}; font-size: {font_size}px;">{text}</span>'
     st.markdown(res, unsafe_allow_html=True )
 
 
+# retrieve model from pickle
+with open('model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+# retrieve encoder from pickle
+with open('label_encoder.pkl', 'rb') as f:
+    label_encoder = pickle.load(f)
+
     
+with open('target_cities.json') as json_file:
+        target_cities = json.load(json_file)
+        
+        
 with st.form(key="user_inputs"):
     print_fancy_header(text='\nHere you can choose which city to process.',
                        font_size=24, color="#00FFFF")
     st.write("🗨 Wait for the map to load, then click on the desired city to select. Now click the 'Submit' button.")
 
-    my_map = folium.Map(location=[39.8283, -98.5795], zoom_start=4)
-    
-    cities = {
-        'Seattle': [47.6062, -122.3321],
-        'New York City': [40.7128, -74.0060],
-        'Los Angeles': [34.0522, -118.2437],
-        'Chicago': [41.8781, -87.6298],
-        'Houston': [29.7604, -95.3698]
-    }
+    my_map = folium.Map(location=[42.57, -44.092], zoom_start=2)
 
+
+    with open('dict_for_map.json') as json_file:
+        dict_for_map = json.load(json_file)
 
     # Add markers for each city
-    for city, coord in cities.items():
-        folium.CircleMarker(
-            location=coord,
-            popup=city
-        ).add_to(my_map)
+    for continent in target_cities:
+        for city_name, coords in target_cities[continent].items():
+            folium.CircleMarker(
+                location=coords,
+                popup=city_name
+            ).add_to(my_map)
 
     my_map.add_child(folium.LatLngPopup())
 
-    res_map = st_folium(my_map, height=300, width=600)
+    res_map = st_folium(my_map, width=640, height=480)
 
     try:
         new_lat, new_long = res_map["last_clicked"]["lat"], res_map["last_clicked"]["lng"]
@@ -48,15 +58,16 @@ with st.form(key="user_inputs"):
 
         # display selected points
         
-        print_fancy_header(text=f"Latitude: {new_lat}", font_size=18, color="#52fa23")
-        print_fancy_header(text=f"Longitude: {new_long}", font_size=18, color="#52fa23")
+        # print_fancy_header(text=f"Latitude: {new_lat}", font_size=18, color="#52fa23")
+        # print_fancy_header(text=f"Longitude: {new_long}", font_size=18, color="#52fa23")
     
         # Calculate the distance between the clicked location and each city
-        distances = {city: distance.distance(coord, (new_lat, new_long)).km for city, coord in cities.items()}
+        distances = {city: distance.distance(coord, (new_lat, new_long)).km for city, coord in dict_for_map.items()}
 
         # Find the city with the minimum distance and print its name
         nearest_city = min(distances, key=distances.get)
         st.write("The nearest city is:", nearest_city)
+        st.write(label_encoder.transform([nearest_city])[0])
 
     except Exception as err:
         print(err)
